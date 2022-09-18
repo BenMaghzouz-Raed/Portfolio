1f5426a9cd64650f02ff0cc0cf3ba7c5c8a332ed
BMRaed7 <62666504+BenMaghzouz-Raed@users.noreply.github.com>
2022-09-18 15:49:18 +0100

    fix images

--- script.js
+++ script.js
@@ -1,34 +1,4 @@
-function sendMail(name, email, subject, message) {
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    myHeaders.set('Authorization', 'Basic ' + btoa('ca27eefbb4f921cdec3dc34d6eae342d'+":" +'63e31e92ee3c2f7aae459930e1b9209f'));
-  
-    const data = JSON.stringify({
-      "Messages": [{
-        "From": {"Email": email, "Name": name},
-        "To": [{"Email": 'raed.maghzouz@gmail.com', "Name": 'Raed'}],
-        "Subject": subject,
-        "TextPart": message
-      }]
-    });
-  
-    const requestOptions = {
-      method: 'POST',
-      mode: 'no-cors',
-      headers: myHeaders,
-      body: data,
-    };
-  
-    fetch("https://api.mailjet.com/v3.1/send", requestOptions)
-      .then(response => response.text())
-      .then(result => console.log(result))
-      .catch(error => console.log('error', error));
-  }
-
-function send(){
-    const name = document.getElementById('name-input').value;
-    const email = document.getElementById('email-input').value;
-    const message = document.getElementById('message-input').value;
-
-    sendMail(name, email, 'Email From Portfolio', message);
-}
+document.getElementById('play').addEventListener('click', function (e) {
+  e.preventDefault();
+  document.getElementById('audio').play();
+});
